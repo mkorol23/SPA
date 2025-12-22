c8a944530c297d1d2701c5fa331e22607b3db6e3
mkorol23 <m.korol@student.sumdu.edu.ua>
2025-12-22 08:17:32 +0200

    PR-01: layout page and connect components

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,20 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+
+import { Header } from './header/header';
+import { Footer } from './footer/footer';
+import { Sidebar } from './sidebar/sidebar';
+import { MainContent } from './main-content/main-content';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [
+    Header,
+    Footer,
+    Sidebar,
+    MainContent
+  ],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
-export class App {
-  protected readonly title = signal('app');
-}
+export class App { }
