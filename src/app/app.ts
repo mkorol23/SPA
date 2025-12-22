2355994f87fd5f613503157a2a14f1722e78e228
mkorol23 <m.korol@student.sumdu.edu.ua>
2025-12-22 09:36:47 +0200

    PR-02: connect user-profile component to app

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,6 +4,8 @@ import { Header } from './header/header';
 import { Footer } from './footer/footer';
 import { Sidebar } from './sidebar/sidebar';
 import { MainContent } from './main-content/main-content';
+import { UserProfile } from './user-profile/user-profile';
+
 
 @Component({
   selector: 'app-root',
@@ -13,6 +15,7 @@ import { MainContent } from './main-content/main-content';
     Footer,
     Sidebar,
     MainContent
+    UserProfile
   ],
   templateUrl: './app.html',
   styleUrl: './app.css',
