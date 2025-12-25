49d0ca6259ce845c55a8d5b402a6b06e1da3369a
mkorol23 <m.korol@student.sumdu.edu.ua>
2025-12-25 23:26:45 +0200

    PR-03: create products-table component

---
+++ src/app/products-table/products-table.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-products-table',
+  imports: [],
+  templateUrl: './products-table.html',
+  styleUrl: './products-table.css',
+})
+export class ProductsTable {
+
+}
