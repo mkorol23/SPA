4c21ee25f0ad3465c374576902f74b3a27f06cd5
mkorol23 <m.korol@student.sumdu.edu.ua>
2025-12-26 00:37:33 +0200

    PR-04: implement question logic and template

--- src/app/question/question.ts
+++ src/app/question/question.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-question',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './question.html',
   styleUrl: './question.css',
 })
-export class Question {
+export class QuestionComponent {
+  @Input() question!: {
+    text: string;
+    options: string[];
+    correctIndex: number;
+  };
 
+  @Output() answerSelected = new EventEmitter<string>();
+
+  selectedIndex: number | null = null;
+  showCorrect = false;
+
+  selectAnswer(index: number) {
+    this.selectedIndex = index;
+    this.answerSelected.emit(this.question.options[index]);
+  }
+
+  showAnswer() {
+    this.showCorrect = true;
+  }
 }
