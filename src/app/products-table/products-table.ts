b8a7d442f2cc3f31c899b7eb1bcd2b1245e6d296
mkorol23 <m.korol@student.sumdu.edu.ua>
2025-12-25 23:43:58 +0200

    PR-03: add products array model

--- src/app/products-table/products-table.ts
+++ src/app/products-table/products-table.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+type Product = {
+  name: string;
+  category: string;
+  price: number;
+  stockCount: number;
+};
 
 @Component({
   selector: 'app-products-table',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './products-table.html',
   styleUrl: './products-table.css',
 })
 export class ProductsTable {
-
+  products: Product[] = [
+    { name: 'Зошит', category: 'Канцтовари', price: 25, stockCount: 12 },
+    { name: 'Рюкзак', category: 'Аксесуари', price: 180, stockCount: 3 },
+    { name: 'Навушники', category: 'Електроніка', price: 320, stockCount: 0 },
+    { name: 'Пляшка для води', category: 'Спорт', price: 70, stockCount: 8 },
+    { name: 'Флешка 64GB', category: 'Електроніка', price: 210, stockCount: 0 },
+  ];
 }
