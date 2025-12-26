d8ae92ebba94974224b859ba01d0b6032c9997c3
mkorol23 <m.korol@student.sumdu.edu.ua>
2025-12-26 03:09:05 +0200

    PR-07: implement switchMap chain for user posts

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,23 +1,27 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-import { AppLogicService } from './app-logic';
-import { DataService } from './data';
+import { BehaviorSubject, switchMap } from 'rxjs';
+import { HttpClientModule } from '@angular/common/http';
+import { ApiService } from './api';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule],
-  providers: [DataService, AppLogicService],
+  imports: [CommonModule, HttpClientModule],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class App {
-  data: string[] = [];
+  users$ = this.api.getUsers();
 
-  constructor(private appLogic: AppLogicService) { }
+  selectedUserId$ = new BehaviorSubject<number>(1);
+  posts$ = this.selectedUserId$.pipe(
+    switchMap(id => this.api.getPostsByUserId(id))
+  );
 
-  loadData() {
-    this.data = this.appLogic.getDataAndLog();
+  constructor(private api: ApiService) { }
+
+  selectUser(id: number) {
+    this.selectedUserId$.next(id);
   }
 }
